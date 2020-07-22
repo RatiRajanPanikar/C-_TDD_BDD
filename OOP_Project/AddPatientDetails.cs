c3d0a2a139c905b3fbe5e0a2a38280c33269293a
ratirr <45217104+ratirr@users.noreply.github.com>
2020-07-21 18:04:19 -0600

    Commit#1

---
+++ OOP_Project/AddPatientDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    class AddPatientDetails
+    {
+        Patient p1 = new Patient(1, "SH789960")
+        {
+
+            InsuranceCompany = "Select Health",
+            PlanName = "Small Employer",
+            
+
+
+
+        };
+        
+            
+
+       
+    }
+}
---
+++ OOP_Project/Patient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    public class Patient
+    {
+        //Empty constructor
+        public Patient()
+        {
+
+        }
+
+        //Constructor with parameter
+        public Patient(int patientID, string _insuranceID)
+        {
+            PatientID = patientID;
+            InsuranceID = _insuranceID;
+
+
+        }
+
+
+        public int PatientID { get; private set; }
+
+        public string InsuranceID { get; private set; }
+
+        public string PlanName { get; set; }
+
+        public string InsuranceCompany { get; set; }
+
+        public static int Count { get; set; }
+
+        private string _displayInsuranceInfo;
+
+        public void CalcTotalServicePaid(int patID)
+        { 
+        var paidByPatient = new List<Double>() { 564.90, 778.0, 677.90 };
+
+        var paidByInsurance = new Double[] { 6577.00, 667.90, 98.90 };
+
+            
+
+
+
+            double TotalPatient = 0;
+            foreach (double pByPat in paidByPatient)
+            {
+
+                TotalPatient += pByPat;
+                //return TotalPatient;
+            }
+
+            double TotalInsurance = 0;
+            foreach (Double pByIns in paidByInsurance)
+            {
+                TotalInsurance += pByIns;
+            }
+            PatientID = patID;
+
+            
+
+            if (patID < 0)
+            {
+                
+            }
+
+        }
+
+        public string DisplayInsuranceInfo {
+
+            get { return "The name of the Insurance Company is: " + InsuranceCompany + " " + "The Plan Type is: " + PlanName; }
+
+            set { _displayInsuranceInfo = value; }
+        }
+
+
+        public Boolean IfNullBlank()
+        {
+            var null_blank = false;
+
+            null_blank = string.IsNullOrWhiteSpace(PlanName)
+                || string.IsNullOrWhiteSpace(InsuranceCompany);
+
+            return null_blank;
+        }
+        //overloading
+        public Patient Display(int PatientID)
+        {
+            return new Patient();
+        }
+
+        //overloading
+        public List<Patient> Display()
+        {
+        
+                 return new List<Patient>();
+        }
+
+       
+        
+        
+
+    }
+}
